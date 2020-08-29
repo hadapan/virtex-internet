41ba123bc4ecba29501efd67885ff711be3612de
hadapan <69392219+hadapan@users.noreply.github.com>
2020-08-29 16:34:35 +0700

    Create virtex.py

---
+++ virtex.py
@@ -0,0 +1,68 @@
+#Coded By Jejak_Internet
+
+#YouTube Jejak_Internet
+
+#Vortex Indonesia
+
+import os,sys,time
+
+ # SILAHKAN COSTUMISASI SENDIRI YA BOSQ #
+
+def main():
+
+    time.sleep(1)
+
+    os.system ('clear')
+
+    print '[96m'
+
+    os.system ('figlet Virtex Terbaru')
+
+    print'[92m================================='
+
+    print'[97m Author : [96mJejak Internet '
+
+    print'[97m YouTube   : [96mJejak Internet '
+
+    print'[92m================================='
+
+    print'[92m++++++++++++ [97mM E N U [92m++++++++++++'
+
+    print'[92m[[97m1[92m] [97mVirtex Terbaru '
+
+    print'[92m[[91m0[92m] [91mExit '
+
+    gans = raw_input ('[97m==>[93m ')
+
+    if gans in ['1']:
+
+        time.sleep(1)
+
+        os.system ('git clone https://github.com/ArroKM/virtexV2 ')
+
+        print 
+
+        print (' [+] Silakan ketik cd virtexV2')
+
+        print (' [+] Silakan ketik python sh virtexV2.sh ')
+
+        exit()
+
+    if gans in ['0']:
+
+        time.sleep(2)
+
+        exit()
+
+    else:
+
+        time.sleep(1)
+
+        print '[97m Pilih Yang Bener [92mBro . . .'
+
+        time.sleep(1)
+
+        main()
+
+main()
+
